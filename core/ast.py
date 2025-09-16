cde3685c844fe094bdd70df6ab303cd01c5ec8d9
MykhayloVoroshylov <161176411+MykhayloVoroshylov@users.noreply.github.com>
2025-09-16 15:24:46 -0400

    Update ast.py

--- core/ast.py
+++ core/ast.py
@@ -42,47 +42,52 @@ class KozakFunctionCall:
 class KozakReturn:
     value: object
 
-class KozakNumber:
-    def __init__(self, value): self.value = value
-    def __repr__(self): return f"KozakNumber({self.value})"
-
-class KozakString:
-    def __init__(self, value):
-        self.value = value
-    def __repr__(self):
-        return f'KozakString({self.value!r})'
-
-class KozakVariable:
-    def __init__(self, name): self.name = name
-    def __repr__(self): return f"KozakVariable({self.name})"
-
-class KozakBinOp:
-    def __init__(self, left, op, right):
-        self.left, self.op, self.right = left, op, right
-    def __repr__(self): return f"KozakBinOp({self.left}, {self.op}, {self.right})"
-
-class KozakAssign:
-    def __init__(self, name, expr):
-        self.name, self.expr = name, expr
-    def __repr__(self): return f"KozakAssign({self.name}, {self.expr})"
-
-class KozakProgram:
-    def __init__(self, statements): self.statements = statements
-    def __repr__(self): return f"KozakProgram({self.statements})"
-
+@dataclasses.dataclass
 class KozakEcho:
-    def __init__(self, expr): self.expr = expr
-    def __repr__(self): return f"KozakEcho({self.expr})"
+    expressions: list
 
+@dataclasses.dataclass
+class KozakBinOp:
+    left: object
+    op: str
+    right: object
+
+@dataclasses.dataclass
+class KozakNumber:
+    value: object
+
+@dataclasses.dataclass
+class KozakString:
+    value: str
+
+@dataclasses.dataclass
+class KozakVariable:
+    name: str
+
+@dataclasses.dataclass
+class KozakAssign:
+    name: str
+    expr: object
+
+@dataclasses.dataclass
+class KozakProgram:
+    statements: list
+
+@dataclasses.dataclass
 class KozakInput:
-    def __init__(self, expr): self.expr = expr
-    def __repr__(self): return f"KozakInput({self.expr})"
+    expr: object
 
+@dataclasses.dataclass
 class KozakBoolean:
-    def __init__(self, value): self.value = value
-    def __repr__(self): return f"KozakBoolean({self.value})"
+    value: bool
 
+@dataclasses.dataclass
 class KozakComparisonOp:
-    def __init__(self, left, op, right):
-        self.left, self.op, self.right = left, op, right
-    def __repr__(self): return f"KozakComparisonOp({self.left}, {self.op}, {self.right})"
+    left: object
+    op: str
+    right: object
+
+@dataclasses.dataclass
+class KozakTypeCast:
+    target_type: str
+    expr: object
