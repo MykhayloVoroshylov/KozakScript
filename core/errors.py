9923bb25eed67b09fd415c5799c9ef7d00fd7fe3
MykhayloVoroshylov <161176411+MykhayloVoroshylov@users.noreply.github.com>
2025-09-19 15:29:43 -0400

    Add files via upload

---
+++ core/errors.py
@@ -0,0 +1,18 @@
+class KozakError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class KozakSyntaxError(KozakError):
+    pass
+
+class KozakTypeError(KozakError):
+    pass
+
+class KozakNameError(KozakError):
+    pass
+
+class KozakValueError(KozakError):
+    pass
+
+class KozakValueError(KozakError):
+    pass
